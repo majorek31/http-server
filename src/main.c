8a7df0f1b393e63d2f59c2ea365e753646122c1a
majorek31 <m4y0r.offical@gmail.com>
2023-08-20 22:23:55 +0200

    feat: send response with default page to any client

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <server.h>
 #include <request.h>
 #include <http_types.h>
+#include <response.h>
 
 int main(int argc, char** argv) {
     if (start_server(6969) != 0) {
@@ -16,6 +17,12 @@ int main(int argc, char** argv) {
     printf("method: %d file: %s\n", req.method, req.requestedFile);
     printf("User agent: '%s'\n", get_header(req.headers, "User-Agent"));
     free_headers(req.headers);
+    
+    struct response_t* response = create_response();
+    set_response_data(response, "<h1>http-server</h1><p>This http server has been created in C, click <a href=\"https://github.com/majorek31/http-server\">here</a> to see it's source code</p>", OK);
+    send_response(fd, response);
+    free_response(response);
+    
     stop_server();
     return 0;
 }
